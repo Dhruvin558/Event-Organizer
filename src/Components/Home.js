4597f6257cbf276268a02c86b4dc4d2659702b39
Dhruvin558 <dhruvinmaniya0558@gmail.com>
2024-09-27 14:41:21 +0530

    .

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
      <div className="home-content" id="home">
         {/* <h1>hello</h1> */}
         <div className="bg">
-          <img src="\image\Llogo.png" className="Logo" />
+          <img src="\image\Llogo.png" className="Logo" alt="logo" />
           <br/>
           {/* <h1 className="logoname">COLD PLAY</h1> */}
         </div>
